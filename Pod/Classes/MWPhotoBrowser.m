b6bfced20dcee7a69dee3aba675ad9fb09afd63d
Eason <lintaodd@163.com>
2018-08-13 17:49:26 +0800

    Update MWPhotoBrowser.m

--- Pod/Classes/MWPhotoBrowser.m
+++ Pod/Classes/MWPhotoBrowser.m
@@ -1087,7 +1087,7 @@ static void * MWVideoPlayerObservation = &MWVideoPlayerObservation;
             }
             self.title = [NSString stringWithFormat:@"%lu %@", (unsigned long)numberOfPhotos, photosText];
         }
-    } else if (numberOfPhotos > 1) {
+    } else if (numberOfPhotos > 0) {
         if ([_delegate respondsToSelector:@selector(photoBrowser:titleForPhotoAtIndex:)]) {
             self.title = [_delegate photoBrowser:self titleForPhotoAtIndex:_currentPageIndex];
         } else {
